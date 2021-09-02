5e6ac75e6daa3d55e6989607103966f682af3dd8
DevJunior21 <junior-wmw@hotmail.com>
2021-09-02 15:04:39 -0400

    Aprofundando a respeito de HoFs

---
+++ High Order Functions/Test.js
@@ -0,0 +1,6 @@
+const wakeUp = () => 'Acordando';
+const coffe = () => 'Bora tomar café'
+const sleep = () => 'Partiu dormir'
+const doigThings = (Func) => console.log(Func);
+
+doigThings(wakeUp())
